270058bed01f5cb2cfd91ceec22f3ffe910c0309
Jahid <114761216+jahid97@users.noreply.github.com>
2025-11-29 23:13:11 +0600

    Refactor server start to use environment port

--- index.js
+++ index.js
@@ -111,4 +111,8 @@ app.use((err, req, res, next) => {
 });
 
 // --- Start server
-app.listen(3000, () => console.log("Server started on port 3000 and base link http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
